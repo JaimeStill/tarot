b154d6438f61650a39609c4659bcc8d800e1ff00
Jaime Still <jpstill85@gmail.com>
2023-01-06 17:22:28 -0500

    do not use an empty base route

--- app/src/app/routes/index.ts
+++ app/src/app/routes/index.ts
@@ -12,6 +12,7 @@ export const Routes = [
 export const RoutePaths: Route[] = [
     { path: 'major/:name', component: MajorRoute },
     { path: 'minor/:name', component: MinorRoute },
-    { path: '', component: TarotRoute },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: 'tarot', component: TarotRoute },
+    { path: '', redirectTo: 'tarot', component: TarotRoute },
+    { path: '**', redirectTo: 'tarot', pathMatch: 'full' }
 ]
